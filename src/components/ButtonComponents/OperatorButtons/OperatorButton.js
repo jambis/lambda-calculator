eefe7d64838dd904ed2a5c11526f9194c27735b9
jambis <imjaamees@gmail.com>
2019-09-10 14:44:08 -0500

    Completed MVP with css

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-const OperatorButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const OperatorButton = ({ operator }) => {
+  return <div className="operator">{operator}</div>;
 };
+
+export default OperatorButton;
